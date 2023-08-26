d2a4f83a5d117733d32f404d9e5132d5320c5d84
sathishsatish <sathishmunusamy12@gmail.com>
2023-08-26 20:06:13 +0530

    code updated

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "terraform-instance" {
   ami           = "ami-065681da47fb4e433" 
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
     tags = {
     Name = "Demo1"
   }
