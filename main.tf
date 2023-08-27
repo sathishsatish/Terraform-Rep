7846746c74c4d5a4235225b8e107c5024e8680ba
sathishsatish <sathishmunusamy12@gmail.com>
2023-08-27 12:27:07 +0530

    Instance Name updated

--- main.tf
+++ main.tf
@@ -3,8 +3,9 @@ resource "aws_instance" "terraform-instance" {
    count = 3  
   ami           = "ami-065681da47fb4e433" 
   instance_type = "t3.micro"
-    tags = {
-    Name = "Demo1"
+   
+  tags = {
+    Name = "Demo2-${count.index}"
   }
 }
 
