0c9dd3f1e9beb2e0821b51c32cff9336257eb4ad
sathishsatish <sathishmunusamy12@gmail.com>
2023-09-15 17:20:26 +0530

    policy attached to user

--- Module-IAM USER/main.tf
+++ Module-IAM USER/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_user" "iam-user"{
     name = var.user_name
-  #  policy_arn = module.TF-Module-IAM Policy.example_policy
+    policy_arn = module.TF-Module-IAM Policy.example_policy
 }
 
