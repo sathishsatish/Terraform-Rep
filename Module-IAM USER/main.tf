8e8a34472c1b9842948b87c3690e41fc3f3848ef
sathishsatish <sathishmunusamy12@gmail.com>
2023-09-17 12:35:01 +0530

    iam user updated

--- Module-IAM USER/main.tf
+++ Module-IAM USER/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_user" "iam-user"{
-  #  name = var.user_name
-    policy_arn = module.TF-Module-IAM Policy.example_policy
+   name = var.user_name
+  #  policy_arn = module.TF-Module-IAM Policy.example_policy
 }
 
