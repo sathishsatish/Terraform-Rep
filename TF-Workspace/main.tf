3394fb00137bf8a2dd71258653b583d5c8e9c95d
sathishsatish <sathishmunusamy12@gmail.com>
2023-09-19 12:17:45 +0530

    provider updated

--- TF-Workspace/main.tf
+++ TF-Workspace/main.tf
@@ -1,12 +1,10 @@
-provider "aws" {
-  region = "us-west-1" 
-}
+
 
 #create a Instance
 resource "aws_instance" "terraform-instance" {
 
-    ami           = "ami-03f2f5212f24db70a" 
-    instance_type = "t2.micro"
+    ami           = "ami-073e64e4c237c08ad" 
+    instance_type = var.instance_type
   
     tags = {
       Name = local.instance_name
