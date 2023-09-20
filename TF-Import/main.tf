0888cfdcda8daaebbca2695829ad629b83d37677
sathishsatish <sathishmunusamy12@gmail.com>
2023-09-20 14:22:20 +0530

    instance type

--- TF-Import/main.tf
+++ TF-Import/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "manual-ec2-import" {
   ami           = "ami-073e64e4c237c08ad" 
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
  
   tags = {
     Name = "manual-ec2-import"
