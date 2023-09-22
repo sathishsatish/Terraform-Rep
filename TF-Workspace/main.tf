ac1f6b73ff4fc1c5739c7d4b8ada3ec0aa37a26f
sathishsatish <sathishmunusamy12@gmail.com>
2023-09-22 16:05:01 +0530

    s3 name updated

--- TF-Workspace/main.tf
+++ TF-Workspace/main.tf
@@ -1,6 +1,4 @@
-/*
 resource "aws_instance" "terraform-instance" {
-
     ami           = "ami-073e64e4c237c08ad" 
     instance_type = var.instance_type
   
@@ -12,7 +10,7 @@ resource "aws_instance" "terraform-instance" {
 locals {
      instance_name = "${terraform.workspace}-instance"
 }
-*/
+
 
 terraform {
 	backend "s3" {
@@ -29,4 +27,9 @@ terraform {
 #  terraform workspace select prodd / testt
 #  terraform plan -var-file="prodd.tf" / "testt.tf"
 #  terraform apply -var-file="prodd.tf" / "testt.tf"
+
+# git pull >> terraform init >> terraform plan cammand >> terraform apply cammand
+
+
 #  aws s3api create-bucket --bucket my-unique-tf-workspace-backup --region us-west-1 --create-bucket-configuration LocationConstraint=us-west-1
+# git pull >> terraform init >> 
